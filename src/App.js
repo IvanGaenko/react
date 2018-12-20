604269eca7662062ef8ae5d770fbbf8ba8ce448a
Ivan Gaenko <gaenko37@gmail.com>
2018-12-20 12:49:03 +0200

    task 2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@
 /* eslint-disable react/jsx-filename-extension */
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 
 import './App.css';
-import Header from './containers/Header';
-import Main from './containers/Main';
 import storeCreator from './store';
+import Layout from './Layout';
 
 let store = storeCreator();
 
@@ -15,12 +13,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <BrowserRouter>
-          <div>
-            <Header />
-            <Main />
-          </div>
-        </BrowserRouter>
+        <Layout />
       </Provider>
     );
   }
