552a0891eebe2629aa9ebf183eeb66e7e353f723
Ivan Gaenko <gaenko37@gmail.com>
2018-12-27 23:59:11 +0200

    add localstorage cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import throttle from 'lodash/throttle';
 
 import './App.css';
 import storeCreator from './store';
 import Layout from './Layout';
+import { saveState } from './localstorage/localstorage';
 
 const store = storeCreator();
+
+store.subscribe(throttle(() => {
+  saveState({ Cart: store.getState().Cart });
+}, 1000));
 
 class App extends Component {
   render() {
--- src/store.js
+++ src/store.js
@@ -2,6 +2,9 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from "redux-thunk";
 
 import reducer from './reducers';
+import { loadState } from './localstorage/localstorage';
+
+const persistedState = loadState();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default () => createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+export default () => createStore(reducer, persistedState, composeEnhancers(applyMiddleware(thunk)));
